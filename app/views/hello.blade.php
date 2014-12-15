44cbb4930a079df95be02432e1f4240ba98eec09
tgrueter <grueth01@gettysburg.edu>
2014-12-14 21:58:32 -0500

    helllllooooo
    sample splash page

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -1,11 +1,17 @@
 @extends('layouts.landing')
 
-@section('header')
- Hello 
-
+@section('header') 
+Welcome {{Auth::user()->first}} !
 @stop
 
 @section('content')
 
-<h1> Welcome {{Auth::user()->first}} </h1>
+
+<p> 	This web page serves as a dashboard for your CPS Project Leon profile. The navigation bar located on the left side of the screen allows you to make donations for the upcoming auction and view your donations made in previous auctions. If you have any questions please contact Jeffrey Rioux at jrioux@gettysburg.edu or contact CPS. Thanks! </p>
+<br>
+<h>	The Center for Public Service</p>
+<p> Gettysburg College, Box 2456 </p>
+<p> Gettysburg, PA  17325-1486 </p>
+<p> Phone: 717-337-6490 </p>
+
 @stop
