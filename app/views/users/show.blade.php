37b6ee9b8a4a408768221345437e34004cf69749
deepthought6x942 <robijo04@gettysburg.edu>
2014-12-15 17:49:32 -0500

    Conflict Resolution 2

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -5,54 +5,6 @@
 @section('content')
 <div class="col-lg-8">
   {{Form::model($user, array('method'=>'PUT', 'route' => array('users.update', $user->id)))}}
-
-<<<<<<< Updated upstream
-  <div class="panel panel-default">
-    <div class="table-responsive">
-      <table class="table">
-        <tbody>
-          <tr>
-            <td>{{ Form::label('first', 'First Name: ')}}</td>
-            <td>{{ Form::text('first')}}</td>
-            <td>{{ $errors->first('first') }}</td>
-          </tr>
-          <tr>
-            <td>{{ Form::label('last', 'Last Name: ')}}</td>
-            <td>{{ Form::text('last')}}</td>
-            <td>{{ $errors->first('last') }}</td>
-          </tr>
-          <tr>
-            <td>{{ Form::label('email', 'E-mail: ')}}</td>
-            <td>{{ Form::text('email')}}</td>
-            <td>{{ $errors->first('email') }}</td>
-          </tr>
-          <tr>
-            <td>{{ Form::label('address1', 'Address One: ')}}</td>
-            <td>{{ Form::text('address1')}}</td>
-            <td>{{ $errors->first('address1') }}</td>
-          </tr>
-          <tr>
-            <td>{{ Form::label('address2', 'Address Two: ')}}</td>
-            <td>{{ Form::text('address2')}}</td>
-            <td>{{ $errors->first('address2') }}</td>
-          </tr>
-          <tr>
-            <td>{{ Form::label('city', 'City: ')}}</td>
-            <td>{{ Form::text('city')}}</td>
-            <td>{{ $errors->first('city') }}</td>
-          </tr>
-          <tr>
-            <td>{{ Form::label('state', 'State: ')}}</td>
-            <td>{{ Form::text('state')}}</td>
-            <td>{{ $errors->first('state') }}</td>
-          </tr>
-          <tr>
-            <td>{{ Form::label('zip', 'Zipcode: ')}}</td>
-            <td>{{ Form::text('zip')}}</td>
-            <td>{{ $errors->first('zip') }}</td>
-          </tr>
-          @if(Auth::user()->type!=='member')
-=======
 <div class="panel panel-default">
   <div class="table-responsive">
     <table class="table">
@@ -98,7 +50,6 @@
           <td>{{ $errors->first('zip') }}</td>
         </tr>
         @if(Auth::user()->type!=='member')
->>>>>>> Stashed changes
           <tr>
             <td>{{Form::label('type', 'Type: ')}}
              <td>{{ Form::select('type', ['administrator'=>'administrator', 'treasurer'=>'treasurer','member'=>'member'])}}</td>
